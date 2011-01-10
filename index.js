b342999a7a4c5638eb7e3f6fd00a0e2bba99f56a
jrburke <jrburke@gmail.com>
2011-01-10 10:19:37 -0800

    small update

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 require(['sys', 'http'], function (sys, http) {
     http.createServer(function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Hello World\n');
+        res.end('Hello World 2\n');
     }).listen(8000);
 });

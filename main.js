22118631999b55491e7b032b6229ac0ea9d2eb7d
jrburke <jrburke@gmail.com>
2011-01-10 10:44:04 -0800

    Using improved r.js renamed to server.js that will automatically look for a main.js in the current file.

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ require(['sys', 'http'], function (sys, http) {
     http.createServer(function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end('Hello World 2\n');
-    }).listen(8000);
+    }).listen(8000, 'r.no.de');
 });

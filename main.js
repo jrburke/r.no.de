2d010ec2e4e4aad8bc44739124fe9271557755d9
jrburke <jrburke@gmail.com>
2011-01-10 10:48:48 -0800

    update comment so I remember next time.

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ require(['sys', 'http'], function (sys, http) {
     http.createServer(function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end('Hello World\n');
+        //port is likely 80 on no.de, on local dev box, 8000
     }).listen(process.env.PORT || 8000);
 });
